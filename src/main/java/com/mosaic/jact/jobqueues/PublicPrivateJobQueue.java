278b19fb775174772a0e1d2ad8f5034eecfd362f
Chris Kirk <ck@tbone-tv.com>
2012-07-10 08:21:19 +0100

    rewrote scheduler performance test; so it now keeps running and prints out its throughput as it goes

---
+++ src/main/java/com/mosaic/jact/jobqueues/PublicPrivateJobQueue.java
@@ -0,0 +1,7 @@
+package com.mosaic.jact.jobqueues;
+
+/**
+ * A job queue that pulls work from two separate job queues.
+ */
+public class PublicPrivateJobQueue {
+}
--- src/test/java/com/mosaic/jact/schedulers/MultiThreadedSchedulerPerfTests.java
+++ src/test/java/com/mosaic/jact/schedulers/MultiThreadedSchedulerPerfTests.java
@@ -2,13 +2,11 @@ package com.mosaic.jact.schedulers;
 
 import com.mosaic.jact.AsyncContext;
 import com.mosaic.jact.AsyncJob;
+import com.mosaic.lang.conc.Monitor;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  *
@@ -16,6 +14,9 @@ import java.util.concurrent.TimeUnit;
 @Ignore
 public class MultiThreadedSchedulerPerfTests {
 
+    // 10th July 2012 - public jobqueue               : 15m jobs per second using all 8 cores (fairly consistent)
+    //                  public then private job queue : 52m jobs per second using 6.5 cores   (have seen as much as 122m using 6 cores)
+    //                                                  varies widely (40m to 122m)
 
     @Test
     public void throughputTest() throws InterruptedException {
@@ -23,70 +24,112 @@ public class MultiThreadedSchedulerPerfTests {
 
         scheduler.start();
 
-        int numTopLevelJobs = 100;
+        ProgressSheet[] progressSheets = setupProgressSheets( 20 );
+        startReportingProgress( progressSheets );
 
-        CountDownLatch latch = new CountDownLatch(numTopLevelJobs*2);
+
+        int numTopLevelJobs = 10000;
 
         for ( int i=0; i<numTopLevelJobs; i++ ) {
-            scheduler.schedule( new SumJob(0) );
+            scheduler.schedule( new SumJob(progressSheets[i%progressSheets.length]) );
         }
 
-        boolean wasTriggered = latch.await( 60, TimeUnit.SECONDS );
-//        assertTrue( wasTriggered );
+        Monitor lock = new Monitor();
+        lock.sleep();
     }
 
-//    @Test   -- Runs out of memory
-    public void javaExecutors() throws InterruptedException {
-        ExecutorService s = Executors.newFixedThreadPool(8);
 
-        Runnable r = new SumRunnable(s, 0);
+    private ProgressSheet[] setupProgressSheets( int numProgressSheets ) {
+        ProgressSheet[] sheets = new ProgressSheet[numProgressSheets];
 
-        for ( int i=0; i<100; i++ ) {
-            s.submit( r );
+        for ( int i=0; i<numProgressSheets; i++ ) {
+            sheets[i] = new ProgressSheet();
         }
 
-        CountDownLatch latch = new CountDownLatch(100);
-
-        boolean wasTriggered = latch.await( 60, TimeUnit.SECONDS );
+        return sheets;
     }
 
-    class SumRunnable implements Runnable {
-        private int v = 0;
-        private int level;
-        private ExecutorService s;
-
-        public SumRunnable( ExecutorService s, int level ) {
-            this.s = s;
-            this.level = level;
+    private void startReportingProgress( final ProgressSheet[] progressSheets ) {
+        for ( ProgressSheet progressSheet : progressSheets ) {
+            progressSheet.start();
         }
 
-        public void run() {
-            if ( level < 7 ) {
-                for ( int i=0; i<10; i++ ) {
-                    s.submit( new SumRunnable(s,level+1) );
+        new Thread() {
+            @Override
+            public void run() {
+                Monitor monitor = new Monitor();
+
+                while ( true ) {
+                    monitor.sleep( 10000 );
+
+                    printProgressUpdate();
+                    resetProgressSheets();
                 }
             }
+
+            private void printProgressUpdate() {
+                double jobsPerSecondAverage = 0;
+
+                for ( ProgressSheet progressSheet : progressSheets ) {
+                    jobsPerSecondAverage += progressSheet.ratePerSecond();
+                }
+
+                System.out.println( "Jobs per second = " + jobsPerSecondAverage );
+            }
+
+            private void resetProgressSheets() {
+                for ( ProgressSheet progressSheet : progressSheets ) {
+                    progressSheet.reset();
+                }
+            }
+        }.start();
+    }
+
+
+    static class ProgressSheet {
+        public final AtomicLong jobCount = new AtomicLong(0);
+
+        public final AtomicLong startMillis = new AtomicLong(0);
+        public final AtomicLong endMillis   = new AtomicLong(0);
+
+
+        public void start() {
+            startMillis.compareAndSet( 0, System.currentTimeMillis() );
+        }
+
+        public void stop() {
+            endMillis.compareAndSet( 0, System.currentTimeMillis() );
+        }
+
+        public double ratePerSecond() {
+            long duration = System.currentTimeMillis() - startMillis.get();
+            long count    = jobCount.get();
+
+            return count / (duration/1000.0);
+        }
+
+        public void reset() {
+            jobCount.set(0);
+            startMillis.set( System.currentTimeMillis() );
+        }
+
+        public void inc() {
+            jobCount.incrementAndGet();
         }
     }
 
     private static class SumJob extends AsyncJob {
-        private int v = 0;
-        private int level;
+        private ProgressSheet progressSheet;
 
-        public SumJob(int level) {
-            this.level = level;
+        public SumJob( ProgressSheet progressSheet ) {
+            this.progressSheet = progressSheet;
         }
 
         @Override
         public Object invoke( AsyncContext asyncContext ) throws Exception {
-            v++;
+            progressSheet.inc();
 
-            if ( level < 7 ) {
-                for ( int i=0; i<10; i++ ) {
-                    asyncContext.scheduleLocally( new SumJob(level+1) ); // apx 5m per second per thread
-//                    asyncContext.schedule( new SumJob(level+1) );      // apx 4m per second per thread
-                }
-            }
+            asyncContext.scheduleLocally( this );
 
             return null;
         }
